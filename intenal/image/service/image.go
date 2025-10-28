a2bbbd4bf6ada0bfaf0d02d1defcd79887d5af12
Adam Elmurzaev <adamibnkhavaj@gmail.com>
2025-10-28 19:56:12 +0500

    feat(service): add Image service with UploadImage method

---
+++ intenal/image/service/image.go
@@ -0,0 +1,46 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/ilam072/image-processor/intenal/image/types/domain"
+	"github.com/ilam072/image-processor/pkg/errutils"
+	"io"
+)
+
+type ImageRepo interface {
+	CreateImage(ctx context.Context, image domain.Image) error
+}
+
+type ImageStorage interface {
+	SaveImage(ctx context.Context, name string, file io.Reader) error
+	DeleteImage(ctx context.Context, name string) error
+}
+
+type Image struct {
+	repo    ImageRepo
+	storage ImageStorage
+}
+
+func (i *Image) UploadImage(ctx context.Context, file io.Reader) (string, error) {
+	const op = "service.image.Upload"
+
+	ID := uuid.New()
+	name := fmt.Sprintf("original/%s", ID.String())
+	if err := i.storage.SaveImage(ctx, name, file); err != nil {
+		return "", errutils.Wrap(op, err)
+	}
+
+	image := domain.Image{
+		ID:   ID,
+		Path: name,
+	}
+
+	if err := i.repo.CreateImage(ctx, image); err != nil {
+		_ = i.storage.DeleteImage(ctx, name)
+		return "", errutils.Wrap(op, err)
+	}
+
+	return ID.String(), nil
+}
