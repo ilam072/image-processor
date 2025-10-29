944cfff96bef461d14dd7206ff9c4f118b7ea25d
Adam Elmurzaev <adamibnkhavaj@gmail.com>
2025-10-30 00:43:26 +0500

    feat(task/postgres): add GetTaskByID and UpdateTaskStatus methods

--- intenal/task/repo/postgres/postgres.go
+++ intenal/task/repo/postgres/postgres.go
@@ -2,6 +2,9 @@ package postgres
 
 import (
 	"context"
+	"database/sql"
+	"errors"
+	"github.com/ilam072/image-processor/intenal/task/repo"
 	"github.com/ilam072/image-processor/intenal/types/domain"
 	"github.com/ilam072/image-processor/pkg/errutils"
 	"github.com/wb-go/wbf/dbpg"
@@ -37,4 +40,44 @@ func (r *TaskRepo) CreateTask(ctx context.Context, task domain.Task) (int, error
 	}
 
 	return ID, nil
+}
+
+func (r *TaskRepo) GetTaskByID(ctx context.Context, id int) (domain.Task, error) {
+	const op = "repo.task.GetTaskByID"
+
+	query := `
+		SELECT id, image_id, processed_path, type, status, created_at
+		FROM tasks
+		WHERE id = $1
+	`
+
+	var task domain.Task
+	row := r.db.QueryRowContext(ctx, query, id)
+	if err := row.Scan(
+		&task.ID,
+		&task.ImageID,
+		&task.ProcessedPath,
+		&task.Type,
+		&task.Status,
+		&task.CreatedAt,
+	); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return domain.Task{}, errutils.Wrap(op, repo.ErrTaskNotFound)
+		}
+		return domain.Task{}, errutils.Wrap(op, err)
+	}
+
+	return task, nil
+}
+
+func (r *TaskRepo) UpdateTaskStatus(ctx context.Context, ID int, status domain.TaskStatus) error {
+	const op = "repo.task.UpdateTaskStatus"
+
+	query := `UPDATE tasks SET status = $1 WHERE id = $2`
+
+	if _, err := r.db.ExecContext(ctx, query, status, ID); err != nil {
+		return errutils.Wrap(op, err)
+	}
+
+	return nil
 }
---
+++ intenal/task/repo/repo.go
@@ -0,0 +1,7 @@
+package repo
+
+import "errors"
+
+var (
+	ErrTaskNotFound = errors.New("task not found")
+)
