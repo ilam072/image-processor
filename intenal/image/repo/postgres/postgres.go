1bea5770f5b87b9d141ebddfdaf8d9e36d224ffa
Adam Elmurzaev <adamibnkhavaj@gmail.com>
2025-10-30 00:46:15 +0500

    feat(image/postgres): add GetPathByID and DeleteImageByID methods

--- intenal/image/repo/postgres/postgres.go
+++ intenal/image/repo/postgres/postgres.go
@@ -54,3 +54,42 @@ func (r *ImageRepo) GetImageByID(ctx context.Context, ID uuid.UUID) (domain.Imag
 
 	return image, nil
 }
+
+func (r *ImageRepo) GetPathByID(ctx context.Context, ID uuid.UUID) (string, error) {
+	const op = "repo.image.GetPathByID"
+
+	query := `SELECT original_path FROM images WHERE id = $1`
+
+	var path string
+
+	if err := r.db.QueryRowContext(ctx, query, ID).Scan(&path); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return "", repo.ErrImageNotFound
+		}
+		return "", err
+	}
+
+	return path, nil
+}
+
+func (r *ImageRepo) DeleteImageByID(ctx context.Context, ID uuid.UUID) error {
+	const op = "repo.image.DeleteByID"
+
+	query := `DELETE FROM images WHERE id = $1`
+
+	result, err := r.db.ExecContext(ctx, query, ID)
+	if err != nil {
+		return errutils.Wrap(op, err)
+	}
+
+	rows, err := result.RowsAffected()
+	if err != nil {
+		return errutils.Wrap(op, err)
+	}
+
+	if rows == 0 {
+		return errutils.Wrap(op, repo.ErrImageNotFound)
+	}
+
+	return nil
+}
