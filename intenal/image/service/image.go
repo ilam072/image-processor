5e6fe600766a746e879f4c1d273166bfdb3dd37f
Adam Elmurzaev <adamibnkhavaj@gmail.com>
2025-10-30 00:46:48 +0500

    feat(image/service): add GetPathByID and DeleteImage methods

--- intenal/image/service/image.go
+++ intenal/image/service/image.go
@@ -2,8 +2,10 @@ package service
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"github.com/google/uuid"
+	"github.com/ilam072/image-processor/intenal/image/repo"
 	"github.com/ilam072/image-processor/intenal/types/domain"
 	"github.com/ilam072/image-processor/pkg/errutils"
 	"io"
@@ -11,6 +13,8 @@ import (
 
 type ImageRepo interface {
 	CreateImage(ctx context.Context, image domain.Image) error
+	GetPathByID(ctx context.Context, ID uuid.UUID) (string, error)
+	DeleteImageByID(ctx context.Context, ID uuid.UUID) error
 }
 
 type ImageStorage interface {
@@ -19,8 +23,12 @@ type ImageStorage interface {
 }
 
 type Image struct {
-	imageRepo ImageRepo
-	storage   ImageStorage
+	repo    ImageRepo
+	storage ImageStorage
+}
+
+func New(repo ImageRepo, storage ImageStorage) *Image {
+	return &Image{repo: repo, storage: storage}
 }
 
 func (i *Image) UploadImage(ctx context.Context, file io.Reader, ext string) (string, error) {
@@ -37,10 +45,46 @@ func (i *Image) UploadImage(ctx context.Context, file io.Reader, ext string) (st
 		Path: name,
 	}
 
-	if err := i.imageRepo.CreateImage(ctx, image); err != nil {
+	if err := i.repo.CreateImage(ctx, image); err != nil {
 		_ = i.storage.DeleteImage(ctx, name)
 		return "", errutils.Wrap(op, err)
 	}
 
 	return ID.String(), nil
+}
+
+func (i *Image) GetPathByID(ctx context.Context, ID uuid.UUID) (string, error) {
+	const op = "service.image.GetPathByID"
+
+	path, err := i.repo.GetPathByID(ctx, ID)
+	if err != nil {
+		if errors.Is(err, repo.ErrImageNotFound) {
+			return "", errutils.Wrap(op, domain.ErrImageNotFound)
+		}
+		return "", errutils.Wrap(op, err)
+	}
+
+	return path, nil
+}
+
+func (i *Image) DeleteImage(ctx context.Context, ID uuid.UUID) error {
+	const op = "service.image.Delete"
+
+	path, err := i.repo.GetPathByID(ctx, ID)
+	if err != nil {
+		if errors.Is(err, repo.ErrImageNotFound) {
+			return errutils.Wrap(op, domain.ErrImageNotFound)
+		}
+		return errutils.Wrap(op, err)
+	}
+
+	if err := i.storage.DeleteImage(ctx, path); err != nil {
+		return errutils.Wrap(op, err)
+	}
+
+	if err := i.repo.DeleteImageByID(ctx, ID); err != nil {
+		return errutils.Wrap(op, err)
+	}
+
+	return nil
 }
